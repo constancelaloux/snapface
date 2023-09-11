2fe63d87d33cf6342988780890ba40653cb5dd7b
Constance Laloux <constancelaloux@gmail.com>
2023-09-11 19:20:01 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { interval, map, filter } from 'rxjs';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit(): void {}
+  interval$!: Observable<string>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      //On a que les valeurs divisibles par 3 qui passent
+      //Filter les émissions et map pour les transformer
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ? 
+        `Je suis ${value} et je suis pair` : 
+        `Je suis ${value} et je suis impair`)
+    );
+  }
 }
